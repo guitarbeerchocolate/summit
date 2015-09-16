959f396a292ad9f652c00cdcc61ef481807b1bbf
guitarbeerchocolate <guitarbeerchocolate@googlemail.com>
2015-09-16 17:10:39 +0100

    Removed useless files. Updated homepage and custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,4 @@
 (function()
 {
-  /* Start of loading includes */
-  $('nav.navbar.navbar-default').include('includes/navbar.html');
-  $('header').include('includes/header.html');
-  $('footer').include('includes/footer.html');
-  /* End of loading includes */  
+  
 })();
